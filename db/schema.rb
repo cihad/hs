519a57ee6c2d9937fe71059bbfdebdf1dfa74ca8
Cihad <cihadp@gmail.com>
2014-11-02 18:30:55 +0200

    add postgresql database

--- db/schema.rb
+++ db/schema.rb
@@ -13,20 +13,15 @@
 
 ActiveRecord::Schema.define(version: 20141026145529) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "images", force: true do |t|
     t.string   "image"
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  create_table "images_nodes", id: false, force: true do |t|
-    t.integer "image_id", null: false
-    t.integer "node_id",  null: false
-  end
-
-  add_index "images_nodes", ["image_id", "node_id"], name: "index_images_nodes_on_image_id_and_node_id"
-  add_index "images_nodes", ["node_id", "image_id"], name: "index_images_nodes_on_node_id_and_image_id"
 
   create_table "node_images", force: true do |t|
     t.integer  "node_id"
@@ -36,8 +31,8 @@ ActiveRecord::Schema.define(version: 20141026145529) do
     t.datetime "updated_at",             null: false
   end
 
-  add_index "node_images", ["image_id"], name: "index_node_images_on_image_id"
-  add_index "node_images", ["node_id", "position"], name: "index_node_images_on_node_id_and_position"
+  add_index "node_images", ["image_id"], name: "index_node_images_on_image_id", using: :btree
+  add_index "node_images", ["node_id", "position"], name: "index_node_images_on_node_id_and_position", using: :btree
 
   create_table "nodes", force: true do |t|
     t.string   "title"
@@ -48,6 +43,6 @@ ActiveRecord::Schema.define(version: 20141026145529) do
     t.string   "status"
   end
 
-  add_index "nodes", ["status"], name: "index_nodes_on_status"
+  add_index "nodes", ["status"], name: "index_nodes_on_status", using: :btree
 
 end
