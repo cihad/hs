01989369d5dbce462aa71d5f1a29b4d95b23405a
Cihad <cihadp@gmail.com>
2014-11-18 23:55:25 +0200

    remove unnecessary migration

--- db/migrate/20141118193607_recreate_image_thumbnail_versions.rb
+++
@@ -1,7 +0,0 @@
-class RecreateImageThumbnailVersions < ActiveRecord::Migration
-  def change
-    Image.find_each do |image|
-      image.image.recreate_versions! if image.image?
-    end
-  end
-end
