a3fad5db9c3c4d9ebd0d418a02d49b2a7b7ac815
Cihad <cihadp@gmail.com>
2015-04-05 13:42:08 +0300

    fix migrate

--- db/migrate/20150316220327_rename_node_to_content_column.rb
+++ db/migrate/20150316220327_rename_node_to_content_column.rb
@@ -6,20 +6,13 @@ class RenameNodeToContentColumn < ActiveRecord::Migration
     reversible do |dir|
       dir.up do
         puts "running..."
-        ContentImage.all.each do |ci|
-          ci.content_id = Node.find(ci.content_id).content_id
-          ci.save
+        node_ids = ContentImage.all.map(&:content_id)
+        node_ids.each do |node_id|
+          content_id = Node.find(node_id).content_id
+          content_image = ContentImage.find_by content_id: node_id
+          content_image.update content_id: content_id
         end
         say "all 'content_id's refilled by real node_id equivalent to <content object> in content_images table"
-      end
-
-      dir.down do
-        class_name = Object.const_defined?("NodeImage") ? "NodeImage" : "ContentImage"
-        class_name.constantize.all.each do |ni|
-          ni.node_id = Node.find_by(content_id: ni.node_id).id
-          ni.save
-        end
-        say "all 'node_id's refilled by real content_id equivalent to <node object> in node_images table"
       end
     end
   end
