641fc4c6de57500d3867fe2ca739acfb5e687b15
Cihad <cihadp@gmail.com>
2015-07-08 01:10:36 +0300

    fix: unknown provider

--- vendor/assets/plugins/editor/js/controllers/app_controller.js
+++ vendor/assets/plugins/editor/js/controllers/app_controller.js
@@ -1,81 +1,84 @@
-EDITOR.controller('AppController', function($rootScope, $scope, $document, WidgetType) {
-  $scope.WidgetType = WidgetType;
+EDITOR.controller('AppController', ['$rootScope', '$scope', '$document', 'WidgetType', 
+  function($rootScope, $scope, $document, WidgetType) {
 
-  $scope.getNumber = function(number) {
-    return new Array(number);
-  }
+    $scope.WidgetType = WidgetType;
 
-  $document.on('click', function() {
-    $rootScope.$apply(function(){
-      $rootScope.currentWidget = null;
-    });
-    console.log('Clicked Document!');
-  })
-
-  $scope.sortableWidgetOptions = {
-    handle: '.handle',
-    placeholder: 'highlight',
-    axis: 'y' ,
-    start: function(event, ui) {
-      console.log('sorting started');
-      angular.element('.highlight').height(ui.item.height());
+    $scope.getNumber = function(number) {
+      return new Array(number);
     }
-  }
 
-  $scope.addColumn = function(widget, col) {
-    widget.config.columns.push(col);
-    $scope.newCol = {};
-  }
+    $document.on('click', function() {
+      $rootScope.$apply(function(){
+        $rootScope.currentWidget = null;
+      });
+      console.log('Clicked Document!');
+    })
 
-  $scope.setCurrentWidget = function(widget) {
-    console.log('SETTING WIDGET from app_controller');
-    if (widget.name != "Grid") {
+    $scope.sortableWidgetOptions = {
+      handle: '.handle',
+      placeholder: 'highlight',
+      axis: 'y' ,
+      start: function(event, ui) {
+        console.log('sorting started');
+        angular.element('.highlight').height(ui.item.height());
+      }
+    }
+
+    $scope.addColumn = function(widget, col) {
+      widget.config.columns.push(col);
+      $scope.newCol = {};
+    }
+
+    $scope.setCurrentWidget = function(widget) {
+      console.log('SETTING WIDGET from app_controller');
+      if (widget.name != "Grid") {
+        $rootScope.currentWidget = widget;
+      }
+    }
+
+    $scope.removeWidget = function(widget) {
+      index = $scope.widgets.indexOf(widget);
+      if (index > -1) {
+        $scope.widgets.splice(index, 1)
+        console.log('WIDGET REMOVED!');
+      }
+    }
+
+    $scope.previewImage = function(image) {
+      console.log('previewed image');
+    }
+
+    $scope.addWidget = function (widget) {
+      var timestamp = Date.now();
+      var newWidget = angular.copy(widget);
+      newWidget['timestamp'] = timestamp;
+      $scope.widgets.push(newWidget);
+      console.log(widget);
+    }
+
+    $scope.dropCallback = function(event, ui) {
+      sourceWidget = angular.element(ui.draggable).scope().widget;
+      targetScope = angular.element(event.target).scope();
+      targetCol = targetScope.widget;
+      grid = targetScope.$parent.$parent.widget;
+      index = grid.config.columns.indexOf(targetCol);
+      size = targetCol.size;
+      newCol = angular.copy(sourceWidget);
+      newCol.size = size;
+      grid.config.columns[index] = newCol;
+    }
+
+    $scope.setCurrentWidgetFromDirective = function(widget) {
+      console.log('setting current widget from widget view.')
+      console.log(widget);
       $rootScope.currentWidget = widget;
     }
-  }
 
-  $scope.removeWidget = function(widget) {
-    index = $scope.widgets.indexOf(widget);
-    if (index > -1) {
-      $scope.widgets.splice(index, 1)
-      console.log('WIDGET REMOVED!');
+    $scope.oneLineMedium = {
+      "placeholder": false,
+      "paste": { "forcePlainText": true },
+      "disableReturn": true
     }
-  }
 
-  $scope.previewImage = function(image) {
-    console.log('previewed image');
   }
-
-  $scope.addWidget = function (widget) {
-    var timestamp = Date.now();
-    var newWidget = angular.copy(widget);
-    newWidget['timestamp'] = timestamp;
-    $scope.widgets.push(newWidget);
-    console.log(widget);
-  }
-
-  $scope.dropCallback = function(event, ui) {
-    sourceWidget = angular.element(ui.draggable).scope().widget;
-    targetScope = angular.element(event.target).scope();
-    targetCol = targetScope.widget;
-    grid = targetScope.$parent.$parent.widget;
-    index = grid.config.columns.indexOf(targetCol);
-    size = targetCol.size;
-    newCol = angular.copy(sourceWidget);
-    newCol.size = size;
-    grid.config.columns[index] = newCol;
-  }
-
-  $scope.setCurrentWidgetFromDirective = function(widget) {
-    console.log('setting current widget from widget view.')
-    console.log(widget);
-    $rootScope.currentWidget = widget;
-  }
-
-  $scope.oneLineMedium = {
-    "placeholder": false,
-    "paste": { "forcePlainText": true },
-    "disableReturn": true
-  }
-
-});
+]);
--- vendor/assets/plugins/editor/js/directives/header_tag.js
+++ vendor/assets/plugins/editor/js/directives/header_tag.js
@@ -1,41 +1,43 @@
-EDITOR.directive('headerTag', function($compile) {
-  return {
-    restrict: 'EA',
-    replace: true,
-    scope: { 
-      tag: '=',
-      widget: '=',
-      model: '@'
-    },
-    link: function(scope, element, attrs) {
-      var tag = scope.tag,
-          model = scope.model,
-          previousHeader = null;
+EDITOR.directive('headerTag', ['$compile', 
+  function($compile) {
+    return {
+      restrict: 'EA',
+      replace: true,
+      scope: { 
+        tag: '=',
+        widget: '=',
+        model: '@'
+      },
+      link: function(scope, element, attrs) {
+        var tag = scope.tag,
+            model = scope.model,
+            previousHeader = null;
 
-      var template = function(tag, model) {
-        return '<'+ tag +' medium-editor ' +
-          'ng-model="'+ model +'" ' +
-          'ng-class="{ \'bordered-bottom\' : widget.config.bordered }" ' +
-          'options=\'{' +
-            '"forcePlainText": true,' +
-            '"disableReturn": true,' +
-            '"buttons": ["bold", "italic", "underline", "anchor"],' +
-            '"spellcheck": false' + 
-          '}\'>' +
-        '</'+ tag +'>';
+        var template = function(tag, model) {
+          return '<'+ tag +' medium-editor ' +
+            'ng-model="'+ model +'" ' +
+            'ng-class="{ \'bordered-bottom\' : widget.config.bordered }" ' +
+            'options=\'{' +
+              '"forcePlainText": true,' +
+              '"disableReturn": true,' +
+              '"buttons": ["bold", "italic", "underline", "anchor"],' +
+              '"spellcheck": false' + 
+            '}\'>' +
+          '</'+ tag +'>';
+        }
+
+        scope.$watch('tag', function(tag) {
+          if (previousHeader) {
+            previousHeader.remove();
+            previousHeader = null;
+          };
+
+          var view = $compile(template(tag, model))(scope)
+          element.append(view);
+          previousHeader = view;
+        });
+
       }
-
-      scope.$watch('tag', function(tag) {
-        if (previousHeader) {
-          previousHeader.remove();
-          previousHeader = null;
-        };
-
-        var view = $compile(template(tag, model))(scope)
-        element.append(view);
-        previousHeader = view;
-      });
-
     }
   }
-})
+])
