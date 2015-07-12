f19da4a37e2f829104599d6e147a2dd5c0e658ba
Cihad <cihadp@gmail.com>
2015-07-12 18:12:11 +0300

    fill image polymorphic association from body widgets

--- app/models/concerns/imageable.rb
+++ app/models/concerns/imageable.rb
@@ -3,6 +3,33 @@ module Imageable
 
   included do
     has_many :images, as: :imageable
+    after_save :save_images
+  end
+
+  def save_images
+    if self.class.column_names.include? "body_widgets"
+      arr = JSON.parse body_widgets
+      
+      select_image_widgets arr      
+
+      arr.select { |widget| widget.name == "Grid" }.each do |grid|
+        select_image_widgets grid.config.columns
+      end
+    end
+  end
+
+  def select_image_widgets arr
+    arr.select { |widget| widget.name == "Image" }.each do |image_widget|
+      add_imageable_to_image image_widget
+    end
+  end
+
+  def add_imageable_to_image image_widget
+    image = Image.find_by image: image_widget.value.url.split('/').last
+    if image and !image.imageable
+      image.imageable = self 
+      image.save
+    end
   end
 
 end
